b8c797d971aba4092ef717a1851d85fae25d7199
Marco <marcosiegman01@gmail.com>
2020-03-26 15:04:49 -0600

    added KeyIcon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,8 +2,9 @@ import EmailIcon from './Email';
 import LockIcon from './Lock';
 import ToopagoIcon from './ToopagoIcon';
 import CirlcesIcon from './Circles';
+import KeyIcon from './Key';
 
-export { EmailIcon, LockIcon, ToopagoIcon, CirlcesIcon };
+export { EmailIcon, LockIcon, ToopagoIcon, CirlcesIcon, KeyIcon };
 
 import { StyleProp, ViewStyle } from 'react-native';
 export type IconProps = {
