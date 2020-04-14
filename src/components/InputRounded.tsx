cb179d0f9121cca4997ef9095a98a7d44387b6eb
Marco <marcosiegman01@gmail.com>
2020-04-13 20:42:08 -0500

    added ref to input in forwared ref

--- src/components/InputRounded.tsx
+++ src/components/InputRounded.tsx
@@ -15,58 +15,60 @@ const AnimatedInput = Animated.createAnimatedComponent(TextInput);
 
 type InputType = React.FC<TextInputProps & { hasCenterText?: boolean }>;
 
-const InputRounded: InputType = ({ children, style, ...props }) => {
-  //hooks
-  const [animatedValue] = useState(new Animated.Value(1));
-  const theme: ThemeType = useContext(ThemeContext);
-  const interpolatedColor = animatedValue.interpolate({
-    inputRange: [1, 2],
-    outputRange: [theme.gray, theme.secondary],
-  });
+const InputRounded: InputType = React.forwardRef(
+  ({ children, style, ...props }, ref: any) => {
+    const [animatedValue] = useState(new Animated.Value(1));
+    const theme: ThemeType = useContext(ThemeContext);
+    const interpolatedColor = animatedValue.interpolate({
+      inputRange: [1, 2],
+      outputRange: [theme.gray, theme.secondary],
+    });
 
-  const animatedStyles = {
-    borderColor: interpolatedColor,
-    color: interpolatedColor,
-  };
-  const ANIMATION_TIME = 300;
-  //handlers
-  const { onBlur, onFocus } = props;
+    const animatedStyles = {
+      borderColor: interpolatedColor,
+      color: interpolatedColor,
+    };
+    const ANIMATION_TIME = 300;
+    //handlers
+    const { onBlur, onFocus } = props;
 
-  function handleFocus(e: NativeSyntheticEvent<TextInputFocusEventData>) {
-    Animated.timing(animatedValue, {
-      toValue: 2,
-      duration: ANIMATION_TIME,
-      easing: Easing.circle,
-    }).start();
-    if (onFocus) onFocus(e);
+    function handleFocus(e: NativeSyntheticEvent<TextInputFocusEventData>) {
+      Animated.timing(animatedValue, {
+        toValue: 2,
+        duration: ANIMATION_TIME,
+        easing: Easing.circle,
+      }).start();
+      if (onFocus) onFocus(e);
+    }
+    function handleBlur(e: NativeSyntheticEvent<TextInputFocusEventData>) {
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: ANIMATION_TIME,
+      }).start();
+      if (onBlur) onBlur(e);
+    }
+
+    return (
+      <Container style={[style, animatedStyles]}>
+        {children && React.isValidElement(children) && (
+          <IconContainer>
+            {React.cloneElement(children && children, {
+              color: interpolatedColor,
+            })}
+          </IconContainer>
+        )}
+        <StyledInput
+          {...props}
+          ref={ref}
+          maxFontSizeMultiplier={0}
+          style={{ color: interpolatedColor }}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </Container>
+    );
   }
-  function handleBlur(e: NativeSyntheticEvent<TextInputFocusEventData>) {
-    Animated.timing(animatedValue, {
-      toValue: 1,
-      duration: ANIMATION_TIME,
-    }).start();
-    if (onBlur) onBlur(e);
-  }
-
-  return (
-    <Container style={[style, animatedStyles]}>
-      {children && React.isValidElement(children) && (
-        <IconContainer>
-          {React.cloneElement(children && children, {
-            color: interpolatedColor,
-          })}
-        </IconContainer>
-      )}
-      <StyledInput
-        {...props}
-        maxFontSizeMultiplier={0}
-        style={{ color: interpolatedColor }}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-      />
-    </Container>
-  );
-};
+);
 
 const Container = styled(Animated.View)`
   width: 100%;
